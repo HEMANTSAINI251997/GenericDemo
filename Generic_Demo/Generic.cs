dbbab3a788406630e8469ded20f7e6fb2abd06a1
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-30 15:33:44 +0530

    [HEMANT]Add: FIND THE 3 INTEGERS MAXIMUM

---
+++ Generic_Demo/Generic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Demo
+{
+    public class Generic<T>
+    {
+        public T data;
+        public  Generic(T data)
+        {
+            this.data = data;
+            Console.WriteLine("This is Generic data = "+ this.data);
+
+        }
+        public void check(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+    public class Demo
+    {
+        public void checkdemo<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Generic_Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> generic = new Generic<int>(25);
+            Generic<long> genericlong = new Generic<long>(2555555);
+            Generic<float> genericfloat = new Generic<float>(2/5);
+            Generic<string> genericstring = new Generic<string>("hello");
+            Generic<char> genericchar = new Generic<char>('a');
+            Generic<double> genericdouble = new Generic<double>(25.2);
+            Generic<bool> genericbool = new Generic<bool>(true);
+            generic.check(25);
+            genericstring.check("HEMANT");
+            genericlong.check(2555555);
+
+            //object of nromal class
+            Demo demo = new Demo();
+            demo.checkdemo<int>(30);
+            demo.checkdemo<string>("SAINI");
+        }
+    }
+}
